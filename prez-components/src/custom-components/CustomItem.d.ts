3e7084f460890106a8c6474294601f77b65405b8
hjohns <howiejohns@gmail.com>
2024-04-14 23:25:54 +1000

    address build errors

--- prez-components/src/custom-components/CustomItem.d.ts
+++ prez-components/src/custom-components/CustomItem.d.ts
@@ -14,22 +14,3 @@ export type ItemFields = typeof fields[number];
 
 export type ItemProperties = Partial<Record<ItemFields, PrezProperty>>;
 
-function getProps(obj, props) {
-    const result = {};
-    props.forEach(prop => {
-        if (obj.hasOwnProperty(prop)) {
-            result[prop] = obj[prop];
-        }
-    });
-    return result;
-}
-
-export function filterProps(properties: PrezProperties) {
-    return getProps(
-        properties, 
-        [
-            'http://www.opengis.net/def/metamodel/ogc-na/status',
-            'http://www.w3.org/2000/01/rdf-schema#seeAlso'
-        ]
-    )
-}
--- prez-components/src/index.ts
+++ prez-components/src/index.ts
@@ -11,7 +11,6 @@ import PrezUILiteral from "./components/PrezUILiteral.vue";
 import PrezUIBlankNode from "./components/PrezUIBlankNode.vue";
 import CustomCitation from './custom-components/CustomCitation.vue';
 import CustomItem from './custom-components/CustomItem.vue';
-import { filterProps } from "./custom-components/CustomItem.d";
 
 export * from "./types";
 

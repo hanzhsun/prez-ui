87a638476fe672502a05ef72a5ce99592ae2c4bc
hjohns <howiejohns@gmail.com>
2024-04-15 10:28:56 +1000

    fix https doctype url config

--- prez-components/src/custom-components/CustomItem.d.ts
+++ prez-components/src/custom-components/CustomItem.d.ts
@@ -5,7 +5,7 @@ const fields = [
     'http://purl.org/dc/terms/isPartOf',
     'http://purl.org/dc/terms/identifier',
 //    'http://www.w3.org/ns/dcat#Resource',
-    'https://www.opengis.net/def/metamodel/ogc-na/doctype',
+    'http://www.opengis.net/def/metamodel/ogc-na/doctype',
     'http://purl.org/dc/terms/creator',
     'http://purl.org/dc/terms/created',
 ] as const;
